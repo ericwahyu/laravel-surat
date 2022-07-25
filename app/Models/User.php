41fd6e045c2ab761729ce8f569fe24ec7236c91d
erickwahyu19 <erickwahyu19@gmail.com>
2022-07-26 00:00:43 +0700

    eight commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,12 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Support\Collection;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     /**
      * The attributes that are mass assignable.
@@ -57,33 +59,44 @@ class User extends Authenticatable
         return $this->hasMany(Riwayat::class);
     }
 
-    public function isMahasiswa(){
-        $mahasiswa = Mahasiswa::all();
-        $unit_kerja = UnitKerja::all();
+    public function isMahasiswa_or_Dosen(){
+        $mahasiswa = Mahasiswa::findOrFail($this->id);
+        $dosen = Dosen::findOrFail($this->id);
 
-        foreach($mahasiswa as $cek_mahasiswa){
-            if($cek_mahasiswa->user_id == $this->id){
-                $mahasiswa_unitkerja = $cek_mahasiswa->unit_kerja_id;
-                return $mahasiswa_unitkerja;
+        if($dosen){
+            $unit_dosen = Dosenunit::where('dosen_id', $dosen->id)->get();
+            foreach($unit_dosen as $unit){
+                $unit_kerja_dosen[] = $unit->unit_kerja_id;
             }
+            return $unit_kerja_dosen;
         }
     }
 
     public function isAdmin(){
-        $admin = $this->isMahasiswa();
-        if($admin == 4){
-            return true;
-        }else{
-            return false;
+        $admin = $this->isMahasiswa_or_Dosen();
+        $data = count($admin);
+        for($i = 0; $i , $data; $i++){
+            if($admin[$i] == 1){
+                return true;
+                break;
+            }
         }
+
     }
 
     public function isPengelola(){
-        $pengelola = $this->isMahasiswa();
-        if($pengelola == 2){
-            return true;
-        }else{
-            return false;
+        $pengelola = $this->isMahasiswa_or_Dosen();
+        $data = count($pengelola);
+        for($i = 0; $i , $data; $i++){
+            if($pengelola[$i] == 4){
+                return true;
+                break;
+            }
         }
+        // if($pengelola == 4){
+        //     return true;
+        // }else{
+        //     return false;
+        // }
     }
 }
